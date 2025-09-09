4b098c296ef68424d6c26588b4597eee6293f71f
Abhinav mohanan <abhinavmohanan018@gmail.com>
2025-09-09 09:58:19 +0530

    style: make footer responsive

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ export default {
   theme: {
     extend: {
       colors:{
-        customBlue: '#0B60D9'
+        customBlue: '#0B60D9',
+        textBlue:'#0B1263'
+      },
+      keyframes:{
+        slide:{
+          '0%':{top: '-0.5rem'},
+          '100%':{top: '100%'}
+        },
+      },
+      animation:{
+        slide: 'slide 1.5s linear infinite'
       }
     },
   },
